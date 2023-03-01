51683341c0276ffda04d6519d99fec71e270de52
Tias Guns <tias.guns@kuleuven.be>
2023-02-28 22:34:39 -0800

    time_models: change verbosity a bit

--- time_models.py
+++ time_models.py
@@ -12,18 +12,24 @@ from cpmpy.transformations.comparison import only_numexpr_equality
 
 
 def print_model_stats(dirname):
-    times = dict([('flatten', 0.0),
+    times = dict([('pickle', 0.0),
+                  ('flatten', 0.0),
                   ('reify', 0.0),
                   ('numexpr', 0.0),
                   ('bv_implies', 0.0),
+                  ('model', 0.0),
+                  ('solve', 0.0),
                   ('solver', 0.0),
                  ])
-    for f in sorted(glob.glob(join(dirname, "*.bt"))):
+    slowest = ("X", 0.0)
+    for f in sorted(glob.glob(join(dirname, "17b*.bt")))[0:10]:
         try:
             with open(f, 'rb') as fpcl:
                 print('.', end='', flush=True)
+                t0 = time.time()
                 model = pickle.loads(brotli.decompress(fpcl.read()))
                 cpm_cons = model.constraints
+                times['pickle'] += time.time() - t0
 
                 t0 = time.time()
                 cpm_cons = flatten_constraint(cpm_cons)
@@ -38,12 +44,21 @@ def print_model_stats(dirname):
                 times['numexpr'] += time.time() - t0
 
                 t0 = time.time()
-                cpm_cons = only_numexpr_equality(cpm_cons)
+                cpm_cons = only_bv_implies(cpm_cons)
                 times['bv_implies'] += time.time() - t0
+                v = time.time() - t0
+                if v > slowest[1]:
+                    slowest = (f, v)
 
-                m = Model(cpm_cons)
-                sat = m.solve(time_limit=120)
-                times['solver'] += m.status().runtime
+                s = SolverLookup.get("ortools")
+                t0 = time.time()
+                s += cpm_cons
+                times['model'] += time.time() - t0
+
+                t0 = time.time()
+                sat = s.solve(time_limit=120, num_workers=1)
+                times['solve'] += time.time() - t0
+                times['solver'] += s.status().runtime
 
                 if not sat:
                     print(f"\tWARNING, {f} was unsat")
@@ -54,6 +69,7 @@ def print_model_stats(dirname):
     print("")  # after all the .'s
     for (k,v) in times.items():
         print(f"{k}: {v:.2f}")
+    print("Slowest", slowest)
 
 
 if __name__ == '__main__':
