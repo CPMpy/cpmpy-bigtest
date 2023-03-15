d1632d1fce7669a20e872a0334d67d6f9b7f1f93
ignace <ignace.bleukx@kuleuven.be>
2023-03-15 18:44:29 +0100

    added reification morphs added flatten morph before calling other transformations custom error type to catch easier

--- metamorphic_tests.py
+++ metamorphic_tests.py
@@ -11,12 +11,14 @@ from cpmpy.transformations.get_variables import get_variables
 
 from cpmpy.transformations.linearize import linearize_constraint
 
-from cpmpy import *
+import cpmpy as cp
 from cpmpy.expressions.utils import is_any_list
 from cpmpy.transformations.flatten_model import flatten_constraint, normalized_boolexpr, normalized_numexpr, \
     negated_normal
 from cpmpy.transformations.reification import only_bv_implies, reify_rewrite
 from cpmpy.transformations.comparison import only_numexpr_equality
+
+from cpmpy.expressions.globalconstraints import Xor
 
 
 def lists_to_conjunction(cons):
@@ -26,8 +28,16 @@ def lists_to_conjunction(cons):
 
 def metamorphic_test(dirname, solver, iters,fmodels,enb):
     #list of mutators and the amount of constraints they accept.
-    mm_mutators = [(xor_morph),(and_morph),(or_morph),(implies_morph),(not_morph),
-                   (negated_normal_morph),(linearize_constraint_morph), (flatten_morph), (only_numexpr_equality_morph), (normalized_numexpr_morph), (normalized_boolexpr_morph)]
+    mm_mutators = [xor_morph, and_morph, or_morph, implies_morph, not_morph,
+                   negated_normal_morph,
+                   linearize_constraint_morph,
+                   flatten_morph,
+                   only_numexpr_equality_morph,
+                   normalized_numexpr_morph,
+                   normalized_boolexpr_morph,
+                   reify_rewrite_morph,
+                   only_bv_implies_morph,
+                   add_solution]
     #mm_mutators = [add_solution]
     # choose a random model
     f = random.choice(fmodels)
@@ -38,7 +48,7 @@ def metamorphic_test(dirname, solver, iters,fmodels,enb):
         # replace lists by conjunctions
         cons = lists_to_conjunction(cons)
         assert (len(cons)>0), f"{f} has no constraints after l2conj"
-        assert (Model(cons).solve()), f"{f} is not sat"
+        assert (cp.Model(cons).solve()), f"{f} is not sat"
         mutators = []
         for i in range(iters):
             # choose a metamorphic mutation
@@ -48,19 +58,19 @@ def metamorphic_test(dirname, solver, iters,fmodels,enb):
             mutators += [m]
             try:
                 cons += m(cons)  # apply a metamorphic mutation
-            except Exception as exc:
+            except MetamorphicError as exc:
                 enb += 1
-                function, argument = exc.args
-                filename = "internalfunctioncrash"+str(enb)
+                function, argument, e = exc.args
+                filename = "internalfunctioncrash"+str(enb)+".pickle"
                 with open(filename, "wb") as ff:
-                    pickle.dump([function, argument, originalmodel], file=ff) #log function and arguments that caused exception
+                    pickle.dump([function, argument, originalmodel, e], file=ff) # log function and arguments that caused exception
                 print('IE', end='', flush=True)
                 return False # no need to solve model we didn't modify..
 
 
         # enough mutations, time for solving
         try:
-            model = Model(cons)
+            model = cp.Model(cons)
             sat = model.solve(solver=solver, time_limit=20)
             if model.status().runtime > 15:
                 # timeout, skip
@@ -79,8 +89,8 @@ def metamorphic_test(dirname, solver, iters,fmodels,enb):
 
         # if you got here, the model failed...
         enb += 1
-        with open("lasterrormodel" + str(enb), "wb") as f:
-            pickle.dump(model, originalmodel, mutators, file=f)
+        with open("lasterrormodel" + str(enb)+".pickle", "wb") as f:
+            pickle.dump([model, originalmodel, mutators], file=f)
         print(model)
         return False
 
@@ -132,24 +142,28 @@ def implies_morph(cons):
 '''CPMPY-TRANSFORMATION MORPHS'''
 
 
-def flatten_morph(cons):
-    n = random.randint(1,len(cons))
-    randcons = random.choices(cons,k=n)
+def flatten_morph(cons, flatten_all=False):
+    if flatten_all is False:
+        n = random.randint(1,len(cons))
+        randcons = random.choices(cons,k=n)
+    else:
+        randcons = cons
     try:
         flatcons = flatten_constraint(randcons)
         return flatcons
     except Exception as e:
-        raise Exception(flatten_constraint,randcons)
+        raise MetamorphicError(flatten_constraint,randcons, e)
 
 
 def only_numexpr_equality_morph(cons,supported=frozenset()):
     n = random.randint(1, len(cons))
     randcons = random.choices(cons, k=n)
+    flatcons = flatten_morph(randcons, flatten_all=True) # only_numexpr_equality requires flat constraints
     try:
-        newcons = only_numexpr_equality(randcons, supported=supported)
+        newcons = only_numexpr_equality(flatcons, supported=supported)
         return newcons
-    except Exception:
-        raise Exception(only_numexpr_equality, randcons)
+    except Exception as e:
+        raise MetamorphicError(only_numexpr_equality, flatcons, e)
 
 
 def normalized_boolexpr_morph(cons):
@@ -157,8 +171,8 @@ def normalized_boolexpr_morph(cons):
     try:
         con, newcons = normalized_boolexpr(randcon)
         return newcons + [con]
-    except Exception:
-        raise Exception(normalized_boolexpr, randcon)
+    except Exception as e:
+        raise MetamorphicError(normalized_boolexpr, randcon, e)
 
 def normalized_numexpr_morph(cons):
     #TODO should call this on subexpressions, so it's actually called on numexpressions as well
@@ -166,31 +180,52 @@ def normalized_numexpr_morph(cons):
     try:
         con, newcons = normalized_numexpr(randcon)
         return newcons +[con]
-    except Exception:
-        raise Exception(normalized_numexpr, randcon)
+    except Exception as e:
+        raise MetamorphicError(normalized_numexpr, randcon, e)
 
 def negated_normal_morph(cons):
     con = random.choice(cons)
     try:
-        nncon = negated_normal(con)
-        return [~nncon]
+        return [~negated_normal(con)]
     except Exception as e:
-        raise Exception(negated_normal, con)
+        raise MetamorphicError(negated_normal, con, e)
 
 
 def linearize_constraint_morph(cons):
     randcons = random.choices(cons,k=len(cons))
     #only apply linearize after flattening
-    flatcons = flatten_constraint(randcons)
+    flatcons = flatten_morph(randcons, flatten_all=True)
     try:
-        lincons = linearize_constraint(flatcons)
+        return linearize_constraint(flatcons)
     except Exception as e:
-        raise Exception(linearize_constraint, flatcons)
-    return lincons
+        raise MetamorphicError(linearize_constraint, flatcons, e)
+
+def reify_rewrite_morph(cons):
+    randcons = random.choices(cons,k=len(cons))
+    #only apply linearize after flattening
+    flatcons = flatten_morph(randcons, flatten_all=True)
+    try:
+        return reify_rewrite(flatcons)
+    except Exception as e:
+        raise MetamorphicError(reify_rewrite, flatcons, e)
+
+
+def only_bv_implies_morph(cons):
+    randcons = random.choices(cons,k=len(cons))
+    #only apply linearize after flattening
+    flatcons = flatten_morph(randcons, flatten_all=True)
+    try:
+        return only_bv_implies(flatcons)
+    except Exception as e:
+        raise MetamorphicError(only_bv_implies, flatcons, e)
 
 def add_solution(cons):
     vars = get_variables(cons)
     return [var == var.value() for var in vars]
+
+
+class MetamorphicError(Exception):
+    pass
 
 if __name__ == '__main__':
     dirname = "models"
@@ -198,6 +233,8 @@ if __name__ == '__main__':
     iters = 5 # number of metamorphic mutations per model
     sat = True
     enb = 0
+
+    random.seed(0)
     while enb < 10:
         fmodels = glob.glob(join(dirname, "*.bt"))
         sat = metamorphic_test(dirname, solver, iters, fmodels, enb)
