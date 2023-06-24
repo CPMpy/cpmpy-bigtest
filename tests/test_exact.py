89a97d5d4a10333acc2a35be4c78fb94b9e6a4fd
JoD <holkann@gmail.com>
2023-06-24 15:19:14 +0200

    exclude memory outs and unsupported operations

--- tests/test_exact.py
+++ tests/test_exact.py
@@ -10,10 +10,13 @@ import pickle
 import brotli  # install it
 from cpmpy import *
 from cpmpy.transformations.flatten_model import flatten_model
-from cpmpy.exceptions import NotSupportedError
+from cpmpy.exceptions import NotSupportedError, TransformationNotImplementedError
 
 
 MODELS = sorted(glob(join("models", "*.bt")))  # TODO .pcl.bt
+# The following instances are excluded due to Exact requiring too much memory:
+excluded = ["hidato_table","miss_manners16650494454778237.bt","nonogram_regular_table1665051080465427.bt", "pentonomies", "run_nonogram","stable_marriage16650508872192633.bt"]
+MODELS = [x for x in MODELS if all(not x.startswith("models/"+y) for y in excluded)]
 
 @pytest.mark.parametrize("fmodel", MODELS)
 def test_model(fmodel, solver="exact"):
@@ -32,5 +35,5 @@ def test_model(fmodel, solver="exact"):
                 pytest.skip(f"Timeout of {fmodel}: {model.status()}")
 
             assert (sat), f"Model {fmodel} should be SAT"
-        except (NotImplementedError,NotSupportedError):
+        except (NotImplementedError,NotSupportedError,TransformationNotImplementedError):
             pytest.skip(f"{fmodel}: Not Implemented/Supported")
